ac98e1ce530e604a5c7042a88062b75c0a35cc6e
Richard Michael <rmichael@edgeofthenet.org>
2012-02-08 15:01:25 -0500

    JS refactor is complete.

--- public/ether-gist.js
+++ public/ether-gist.js
@@ -1,29 +1,33 @@
 // TODO:
 //
 //   1/ Refactor "etherpadRequest" and "gistRequest" to "jsonRequest"?
-//   2/ Instead of "var foo = function(arg) { ... } ;", use "function foo(arg) { ... };".
-//
+//   3/ Handle GitHub being down..
+//   4/ Handle collaboration ==> ??
+//   5/ Clean out old etherpads ==> session expiry?
+//   6/ Delete pads!
+
+function debugObject(object) {
+  console.log('Debug Object: ' + typeof Object);
+  for (var prop in object) {
+    if (object.hasOwnProperty(prop)) {
+      console.log('Debug Object: (prop) ' + prop + ' = ' + object[prop]);
+    };
+  };
+};
 
 function gistCallback(response) {
 
   // TODO: These variables need to leave this scope.
-
   var meta = response.meta;
   var data = response.data;
-  console.log('GitHub Callback: ' + meta);
-  console.log('GitHub Callback: ' + data);
 };
-
 
 function gistContent(gistId) {
 
-  // TODO: Return values here are a mess.  Needs error handling.
+  // TODO: Return values here need error handling.
 
   var gitHubApiUrl   = 'https://api.github.com';
   var gitHubMimeType = 'application/json';
-
-  // TODO: JSON-P callback seems unnecessary.
-  // var gistUrl = gitHubApiUrl + '/gists/' + gistId + '?callback=gistCallback';
 
   var gistUrl = gitHubApiUrl + '/gists/' + gistId;
 
@@ -43,17 +47,19 @@ function gistContent(gistId) {
       // We can't know the name of the file in the gist, just take the first one.
       if (gist.files.hasOwnProperty(filename)) {
         content = gist.files[filename].content;
-        // break for loop?
+        break; // Break because we want the first element.
       };
     };
   };
 
+  console.debug('In gistContent');
+  console.debug('In gistContent: ' + content);
   return content;
 
 };
 
-
 function etherpadRequest(url) {
+  // Returns an object, eval'd from the response JSON.
 
   var request = new XMLHttpRequest();
 
@@ -70,26 +76,38 @@ function etherpadRequest(url) {
 
 };
 
+function createGroupPad(group, padName, padContent) {
+  //  TODO: Set etherpad code content type for syntax highlighting.
 
-var createGistPad = function(gistId) {
+  var url = etherpadHost + '/api/1/createGroupPad' +
+            '?apikey='   + apiKey +
+            '&groupID='  + group +
+            '&padName='  + padName +
+            '&text='     + padContent;
 
-  var padName    = gistId,                // Gist commit SHA1.
-      padContent = gistContent(gistId);   // Gist commit content.
+  // {code: 0, message:"ok", data: null}
+  // var pad = etherpadRequest(url).data.padID;
+  var pad = etherpadRequest(url);
+  debugObject(pad); // DEBUG.
 
-  var apiKey        = 'EVH6mE9T0LloynagajpPaXpRzPWKlUyJ',
-      etherpadHost  = 'http://localhost:9001',
-      authorName    = 'Michael',               // GitHub username?
-      sessionExpiry = '1328979927',            // 1 day?
 
-      // Gist commit content.
-      // TODO: String properties cannot be accessed using dot-notation,
-      //       e.g. DOES NOT WORK --> "data.files.'test-openssl.rb'.content
-      //
-      // padContent    = gistContent(gistId).data.files.'test-openssl.rb'.content,
-      padContent    = escape(gistContent(gistId));
+  if (pad.code == 0 && pad.message == 'ok') {
 
-      // Gist commit SHA1. // TODO: Delete pads!
-      padName       = gistId + Math.floor(Math.random() * 1000);
+    return pad.data.padID;        // Pad was created. TODO: API says this should be null.
+
+  } else if (pad.code == 1 && pad.message == 'padName does already exist') {
+
+    return group + '$' + padName;  // Pad already exists. API says name format is 'GROUPID$PADNAME'.
+    // return padName;             // Pad already exists. API says name format is 'GROUPID$PADNAME'.
+
+  } else {
+
+    return null;
+  };
+
+};
+
+function createAuthor(authorName) {
 
   // Create an author.  TODO: Check if exists.
   var url = etherpadHost + '/api/1/createAuthorIfNotExistsFor' +
@@ -98,8 +116,12 @@ var createGistPad = function(gistId) {
             '&authorMapper=7';
 
   // {code: 0, message:"ok", data: {authorID: "a.s8oes9dhwrvt0zif"}}
-  var author = etherpadRequest(url).data.authorID;
+  // var author = etherpadRequest(url).data.authorID;
+  return etherpadRequest(url).data.authorID;
 
+};
+
+function createGroup() {
 
   // Create a group.  TODO: Check if exists.
   var url = etherpadHost + '/api/1/createGroupIfNotExistsFor' +
@@ -107,19 +129,11 @@ var createGistPad = function(gistId) {
             '&groupMapper=7';
 
   // {code: 0, message:"ok", data: {groupID: "g.s8oes9dhwrvt0zif"}}
-  var group = etherpadRequest(url).data.groupID;
+  // var group = etherpadRequest(url).data.groupID;
+  return etherpadRequest(url).data.groupID;
+};
 
-
-  // Create the etherpad.  TODO: Check if exists.
-  var url = etherpadHost + '/api/1/createGroupPad' +
-            '?apikey='   + apiKey +
-            '&groupID='  + group +
-            '&padName='  + padName +
-            '&text='     + padContent;
-
-  // {code: 0, message:"ok", data: null}
-  var pad = etherpadRequest(url).data.padID;
-
+function createSession(group, author, expiry) {
 
   // Create the session.  TODO: Check if exists?
   var url = etherpadHost   + '/api/1/createSession' +
@@ -128,32 +142,29 @@ var createGistPad = function(gistId) {
             '&authorID='   + author +
             '&validUntil=' + sessionExpiry;
 
-  var session = etherpadRequest(url).data.sessionID;
+  // var session = etherpadRequest(url).data.sessionID;
+  return etherpadRequest(url).data.sessionID;
+};
 
+function createGistPad(gistId) {
+
+  var padName    = gistId,                       // Gist commit SHA1.
+      padContent = escape(gistContent(gistId)),  // Gist content, escaped.
+      author     = createAuthor(authorName),
+      group      = createGroup(),
+      groupPad   = createGroupPad(group, padName, padContent),
+      session    = createSession(group, author, sessionExpiry);
 
   // Set the document cookie.
   document.cookie = 'sessionID=' + session;
 
-
   // Update the etherpad iframe to point to the new etherpad.
-  var url = etherpadHost + '/p/' + pad;
+  var url = etherpadHost + '/p/' + groupPad;
   document.getElementById('etherpad').setAttribute('src', url);
 
 }; // End createGistPad = function(gistId)
 
-
-// Wire createGistPad() events to all gists.
-var go = function() {
-
-  //  gistImages = document.getElementsByClass('gist-paste');
-
-  //  for (var img in gistImages) {
-  //    img.onClick
-
-  //  };
-
-//  var gistId = '1718696';
-
-}; // End go = function()
-
-window.onload = go;
+var apiKey        = 'EVH6mE9T0LloynagajpPaXpRzPWKlUyJ',
+    etherpadHost  = 'http://localhost:9001',
+    authorName    = 'Michael',               // TODO: GitHub username?
+    sessionExpiry = '1328979927';            // 1 day?
